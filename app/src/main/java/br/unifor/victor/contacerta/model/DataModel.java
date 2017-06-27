ce616b9ae5e744dec3ed1cacce538a1ad8e69764
Jordan Moreira <jordann.chance@gmail.com>
2017-06-27 17:14:21 -0300

    Novas Classes: - Adicionado Adapter para listview da nova activity - Adicionado DataModel para testar listview

---
+++ app/src/main/java/br/unifor/victor/contacerta/model/DataModel.java
@@ -0,0 +1,16 @@
+package br.unifor.victor.contacerta.model;
+
+
+
+public class DataModel {
+
+    public String name;
+    public boolean checked;
+
+    public DataModel(String name, boolean checked) {
+        this.name = name;
+        this.checked = checked;
+
+    }
+
+}
