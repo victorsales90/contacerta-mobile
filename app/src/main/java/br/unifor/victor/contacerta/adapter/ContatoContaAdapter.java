70e6690035d8c75008ed67459055be4da1fcec7f
Victor Sales <victorsales90@gmail.com>
2017-06-28 19:25:17 -0300

    Melhorias no Layout

---
+++ app/src/main/java/br/unifor/victor/contacerta/adapter/ContatoContaAdapter.java
@@ -0,0 +1,57 @@
+package br.unifor.victor.contacerta.adapter;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import br.unifor.victor.contacerta.R;
+import br.unifor.victor.contacerta.model.Contato;
+import br.unifor.victor.contacerta.model.Produto;
+
+/**
+ * Created by victo on 09/06/2017.
+ */
+
+public class ContatoContaAdapter extends ArrayAdapter<Contato> {
+
+    private ArrayList<Contato> contatos;
+    private Context context;
+
+
+    public ContatoContaAdapter(@NonNull Context c, @NonNull ArrayList<Contato> objects) {
+        super(c, 0 , objects);
+        this.contatos = objects;
+        this.context = c;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = null;
+
+        if(contatos != null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+
+            view = inflater.inflate(R.layout.lista_contato_conta, parent, false);
+
+            TextView nomeContato = (TextView) view.findViewById(R.id.tv_nome_contato2);
+          //  TextView valorConta = (TextView) view.findViewById(R.id.tv_valor_conta);
+
+            Contato contato = contatos.get(position);
+
+            nomeContato.setText(contato.getNome());
+         //   valorConta.setText(String.valueOf(contato.getValorConta()));
+
+        }
+
+        return view;
+    }
+}
--- app/src/main/java/br/unifor/victor/contacerta/fragment/ContaFragment.java
+++ app/src/main/java/br/unifor/victor/contacerta/fragment/ContaFragment.java
@@ -7,7 +7,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -15,9 +24,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import br.unifor.victor.contacerta.R;
 import br.unifor.victor.contacerta.activity.MainActivity;
+import br.unifor.victor.contacerta.adapter.ContatoAdapter;
+import br.unifor.victor.contacerta.adapter.ContatoContaAdapter;
 import br.unifor.victor.contacerta.config.ConfiguracaoFirebase;
 import br.unifor.victor.contacerta.helper.Preferencias;
 import br.unifor.victor.contacerta.model.Contato;
@@ -31,15 +43,27 @@ public class ContaFragment extends Fragment {
     private DatabaseReference firebase;
 
     private ArrayList<Produto> produtos;
-    private ArrayList<Contato> contatos;
+   // private ArrayList<Produto> produtos1;
+   private ArrayList<Contato> contatos;
+  //  private ArrayList<Contato> contatos1;
     private TextView txtContaTotal;
     private TextView txtTotalpessoas;
     public double contaTotal;
     private double contaPorPessoa;
+    private ListView listView;
+    private ArrayAdapter adapter;
+    private int numeroPessoasPorPedido;
+    private double contaPessoa2;
+    private double contaPessoa;
+
 
     private  String codContaFirebase;
     private String myDataFromActivity;
 
+    LinearLayout lllistview;
+    LinearLayout lltextview;
+    ToggleButton toggle;
+    Switch switch1;
 
     public ContaFragment() {
         // Required empty public constructor
@@ -55,14 +79,68 @@ public class ContaFragment extends Fragment {
         Log.i("******TELA CONTA FRAG******",codContaFirebase);
 
         firebase = ConfiguracaoFirebase.getFirebase();
+        // instanciar objetos
+        contatos = new ArrayList<>();
+        produtos = new ArrayList<>();
 
         final View view = inflater.inflate(R.layout.fragment_conta, container, false);
+        // monta listview e adapter
+        listView = (ListView) view.findViewById(R.id.lv_contatos_contas);
+        adapter = new ContatoContaAdapter(getActivity(), contatos);
+        listView.setAdapter(adapter);
+
+
+
 
         txtContaTotal = (TextView) view.findViewById(R.id.textViewConta);
         txtTotalpessoas = (TextView) view.findViewById(R.id.textPorPessoa);
 
         Preferencias preferencias = new Preferencias(getActivity());
         String identificadorUsuarioLogado = preferencias.getIdentificador();
+
+        lllistview = (LinearLayout) view.findViewById(R.id.linear_listview);
+        lltextview = (LinearLayout) view.findViewById(R.id.linear_textview_pessoa);
+      //  toggle = (ToggleButton) view.findViewById(R.id.toggleButton);
+        switch1 = (Switch) view.findViewById(R.id.switch1);
+
+
+        lltextview.setVisibility(View.VISIBLE);
+        lllistview.setVisibility(View.GONE);
+
+
+//        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    // The toggle is enabled
+//                    lllistview.setVisibility(View.VISIBLE);
+//                    lltextview.setVisibility(View.GONE);
+//
+//                } else {
+//                    // The toggle is disabled
+//                    lltextview.setVisibility(View.VISIBLE);
+//                    lllistview.setVisibility(View.GONE);
+//
+//                }
+//            }
+//        });
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    lllistview.setVisibility(View.VISIBLE);
+                    lltextview.setVisibility(View.GONE);
+
+                } else {
+                    // The toggle is disabled
+                    lltextview.setVisibility(View.VISIBLE);
+                    lllistview.setVisibility(View.GONE);
+
+                }
+            }
+        });
+
+
 
 
         firebase.child("contas").child(codContaFirebase).child("produtos").addValueEventListener(new ValueEventListener() {
@@ -72,10 +150,10 @@ public class ContaFragment extends Fragment {
 
 
                 for(DataSnapshot dados: dataSnapshot.getChildren()){
-                    Produto produto = dados.getValue(Produto.class);
-                    contaTotal = contaTotal + Double.parseDouble(produto.getValor());
+                    Produto produto1 = dados.getValue(Produto.class);
+                    contaTotal = contaTotal + Double.parseDouble(produto1.getValor());
                     txtContaTotal.setText(String.format("%.2f",(contaTotal)));
-                    Log.i("PRECO DO PRODUTO",produto.getValor());
+                    Log.i("PRECO DO PRODUTO",produto1.getValor());
                     Log.i("SOMA DA CONTA", String.valueOf(contaTotal));
 
                 }
@@ -117,10 +195,97 @@ public class ContaFragment extends Fragment {
             }
         });
 
+        //listview
+        firebase.child("contas").child(codContaFirebase).child("contatos").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // limpar lista
+                contatos.clear();
+
+                // listar contatos
+                for (DataSnapshot dados: dataSnapshot.getChildren()){
+                    Contato contato = dados.getValue(Contato.class);
+                    contatos.add(contato);
+
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                firebase.child("contas").child(codContaFirebase).child("produtos").addListenerForSingleValueEvent(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        for(DataSnapshot dados: dataSnapshot.getChildren()){
+
+                            produtos.clear();
+                            Produto produto = dados.getValue(Produto.class);
+                            produtos.add(produto);
+
+                            String x = contatos.get(position).getIdentificadorUsuario();
+                            String y = produto.getIdPessoaProduto().toString();
+
+                            List<String> ppp = produto.getPessoasPorProdutos();
+                            numeroPessoasPorPedido = ppp.size();
+
+                            //pecorrendo lista de pessoas que compartilham produto
+                            for(int i=0;i<ppp.size();i++){
+                                Log.i("*****VALOR DO LIST******", ppp.get(i).toString());
+
+                                if (x.equals(ppp.get(i).toString())){
+                                    double valorProd = Double.parseDouble(produto.getValor())/numeroPessoasPorPedido;
+                                    contaPessoa2 += valorProd;
+
+                                }
+
+                            }
 
 
 
+                            if (x.equals(y)){
+                                double valorProd = Double.parseDouble(produto.getValor())/numeroPessoasPorPedido;
+                                contaPessoa += valorProd;
+                            }
 
+                        }
+
+                        // contaPessoa2 = contaPessoa2/numeroPessoasPorPedido;
+                        contatos.get(position).setValorConta(contaPessoa2);
+                        Log.i("*****CONTA POR PESSOA nome******", String.valueOf(contatos.get(position).getNome()));
+                        Log.i("*****CONTA POR PESSOA valor******", String.valueOf(contatos.get(position).getValorConta()));
+
+                        Toast.makeText(getActivity(), "A conta da " + contatos.get(position).getNome() + " é " + String.format("%.2f",(contatos.get(position).getValorConta())) , Toast.LENGTH_LONG).show();
+//                        Toast.makeText(getActivity(), "A conta da " + contatos.get(position).getNome() + " é " + contaPessoa , Toast.LENGTH_LONG).show();
+                        contaPessoa = 0.0;
+                        contaPessoa2 = 0.0;
+
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+
+
+                });
+
+
+            }
+
+
+        });
 
 
 
@@ -129,5 +294,11 @@ public class ContaFragment extends Fragment {
         // Inflate the layout for this fragment
         return view;
     }
+
+
+
+
+
+
 
 }
